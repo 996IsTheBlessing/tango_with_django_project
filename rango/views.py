29c25ab413f3a3bd001e7669d97e175836f4fc41
Tao Chen <qq1026264112@outlook.com>
2021-08-06 22:53:55 +0100

    some changes

--- rango/views.py
+++ rango/views.py
@@ -111,23 +111,22 @@ def register(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
 
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
+        if username!="" and email!="" and password!="":
+            user=User.objects.get_or_create(username=username)
+            print(user)
+            if user[1]:
+                user = user[0]
+                user.email=email
+                user.set_password(user.password)
+                print(username, password)
+                user.set_password(password)
+                user.save()
+                profile = UserProfile.objects.create(user=user)
+                registered = True
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
