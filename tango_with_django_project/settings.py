d66864592c74a457b67ca42776397ce45adeee68
Qinxuan Xiong <2548237x@student.gla.ac.uk>
2021-08-06 23:51:08 +0100

    configuration for deployment

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,7 +29,10 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jf0!$yq1u#c#=m9z4-xe)_#+7sa706@x6e!)=e7q*5($+3hedj'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
